787d3de3587bee91accf1e0f6dc47952eb28c84f
Charles Marsaw <marsawjr4@gmail.com>
2025-03-30 13:34:07 -0500

    Added files

---
+++ script.js
@@ -0,0 +1,16 @@
+let count = 0;
+
+document.getElementById("decrease-btn").onclick = function(){
+  count-=1;
+  document.getElementById("counter").innerHTML = count;
+};
+
+document.getElementById("reset-btn").onclick = function(){
+  count=0;
+  document.getElementById("counter").innerHTML = count;
+};
+
+document.getElementById("increase-btn").onclick = function(){
+  count+=1;
+  document.getElementById("counter").innerHTML = count;
+};
